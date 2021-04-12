5904ec72c035c88fe74ceef83485826d27e2f189
deecarneiro <deyse@salvus.me>
2021-04-11 22:31:08 -0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import {View, Text} from 'react-native'
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  async componentDidMount() {
+    
+  }
+
+
+  render() {
+    return (
+      <View>
+      </View>
+   );
+  }
+}
+
+
+export default App;
